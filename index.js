37f99a5f7c76f83c86e4758c506b0358a1817ba6
Mursaleen Taqi <leemursa@gmail.com>
2019-12-10 07:57:15 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+  
+const animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  let animal = 'cat'
+
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+
+  return n + two
+}
